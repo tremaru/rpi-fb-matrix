06ad27c68cbe1ed2124602d26a1be7240cb502b0
drondistortion <cancelbutton@gmail.com>
2019-11-26 16:53:41 +0000

    no bashisms in this household!

--- 2x128.sh
+++ 2x128.sh
@@ -1,1 +1,2 @@
-./rpi-fb-matrix matrix.cfg --led-rgb-sequence=RBG --led-pwm-lsb-nanoseconds=300 --led-pwm-dither-bits=1
+#!/bin/sh
+./$1 matrix.cfg --led-rgb-sequence=RBG --led-pwm-lsb-nanoseconds=300 --led-pwm-dither-bits=1
